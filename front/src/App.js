67ba40bd6c644162f48132e3f33995debfa85675
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-03-30 23:00:27 +0200

    new root react 18

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { EuiProvider } from '@elastic/eui';
+import createCache from '@emotion/cache';
+// import { StrictMode } from 'react';
+// import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+const cache = createCache({
+  key: 'myapp',
+  container: document.querySelector('meta[name="global-style-insert"]'),
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EuiProvider colorMode="dark" cache={cache}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello</h1>
+        </header>
+      </div>
+    </EuiProvider>
   );
 }
-
-export default App;
