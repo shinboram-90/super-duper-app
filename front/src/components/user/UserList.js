8be3a362407625467d8d340079af9b6416abcb34
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-04-13 14:01:05 +0200

    starting profile page

--- front/src/components/user/UserList.js
+++ front/src/components/user/UserList.js
@@ -5,13 +5,14 @@ import UserPage from './UserPage';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   const fetchUsers = async () => {
     try {
       const response = await axios.get('/api/users');
       if (response) {
         console.log(response.data.userList[0]);
-        setUsers(response.data.userList[0]);
+        setIsLoading(false);
+        setUsers(response.data.userList);
       }
     } catch (err) {
       console.error(err);
@@ -21,6 +22,8 @@ const UserList = () => {
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  if (isLoading) return null;
 
   return (
     <>
--- front/src/components/user/UserPage.js
+++ front/src/components/user/UserPage.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import defaultAvatar from '../../assets/defaultAvatar.jpg';
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+import { Avatar } from '@mui/material';
+const UserPage = ({ user }) => {
+  return (
+    <section>
+      <h2>{user.username}</h2>
+      {user.avatar !== null || undefined ? (
+        <Avatar sx={{ width: 100, height: 100 }}>
+          <img alt="avatar" src={user.avatar} crossOrigin="true" />
+        </Avatar>
+      ) : (
+        <Avatar sx={{ width: 100, height: 100 }}>
+          {' '}
+          <img
+            alt="avatar"
+            height="100"
+            src={defaultAvatar}
+            crossOrigin="true"
+          />
+        </Avatar>
+      )}
+      <p>
+        <span>{user.first_name}</span>
+        <span>{user.last_name}</span>
+      </p>
+      <p>{user.email}</p>
+      <p>{user.biography}</p>
+    </section>
+  );
 };
 
 export default UserPage;
